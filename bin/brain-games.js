d2827a2d66640689ce14cf789c6988e715d33b11
vsbdn <v.boldurean@gmail.com>
2020-09-02 18:22:38 +0100

    Added: eslint

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
+import getUserName from '../src/cli.js';
 
-import {getUserName} from "../src/cli.js";
-
-console.log('Welcome to the Brain Games!')
-getUserName()
+console.log('Welcome to the Brain Games!');
+getUserName();
--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
-import readlineSync from 'readline-sync'
+import readlineSync from 'readline-sync';
 
+const getUserName = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hi${userName}!`);
+};
 
-export const getUserName = () => {
-    const userName = readlineSync.question('May I have your name? ');
-    console.log('Hi ' + userName + '!');
-
-}
+export default getUserName();
